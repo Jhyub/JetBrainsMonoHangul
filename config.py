ea423d0102fd0fd080e9f23cb71a1e1f1ac9891a
unta1337 <firstapple42@gmail.com>
2024-02-01 00:44:43 +0900

    Add simple build script
    1. Add build script.
    2. config.py: configurations for build fonts.
    2. hangulify.py: modify variable name accordingly.

---
+++ config.py
@@ -0,0 +1,17 @@
+download_path = 'assets'
+out_path = 'out'
+
+jetbrains_mono_version = '2.304'
+d2_coding_version = '1.3.2'
+d2_coding_date = '20180524'
+
+jetbrains_mono_url = f'https://github.com/JetBrains/JetBrainsMono/releases/download/v{jetbrains_mono_version}/JetBrainsMono-{jetbrains_mono_version}.zip'
+d2_coding_url = f'https://github.com/naver/d2codingfont/releases/download/VER{d2_coding_version}/D2Coding-Ver{d2_coding_version}-{d2_coding_date}.zip'
+
+jetbrains_mono_name = 'JetBrains_Mono.zip'
+d2_coding_name = 'D2_Coding.zip'
+
+d2_coding_width = 1000
+jetbrains_mono_width = 1200
+
+use_system_wget = False
